d71b558c62687408b12b2603c47b09f714dc70ca
Chad Lee <git@chadly.net>
2017-08-12 12:30:09 -0500

    Moving fetch-on-update over from redux-rsi
    See https://github.com/civicsource/redux-rsi

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export checkStatus from "./check-status";
 export parseJSON from "./parse-json";
 export batchFetch from "./batch-fetch";
+export fetchOnUpdate from "./fetch-on-update";
---
+++ test/behaves-like-browser.js
@@ -0,0 +1,31 @@
+import { JSDOM } from "jsdom";
+
+export default function() {
+	// inspired by https://semaphoreci.com/community/tutorials/testing-react-components-with-enzyme-and-mocha
+
+	beforeEach(function() {
+		this.exposedProperties = ["document", "window", "navigator"];
+
+		const dom = new JSDOM("<!DOCTYPE html><p>Hello world</p>");
+		global.document = dom.window.document;
+
+		global.window = dom.window;
+
+		global.navigator = {
+			userAgent: "node.js"
+		};
+
+		Object.keys(document.defaultView).forEach(property => {
+			if (typeof global[property] === "undefined") {
+				this.exposedProperties.push(property);
+				global[property] = document.defaultView[property];
+			}
+		});
+	});
+
+	afterEach(function() {
+		this.exposedProperties.forEach(property => {
+			delete global[property];
+		});
+	});
+}
