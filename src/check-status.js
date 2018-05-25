bac1c93f672c9e5ace759c12d7f9b8ef5e435776
Chad Lee <git@chadly.net>
2018-05-25 11:15:03 -0500

    Expose the logic for extracting error messages
    The library now exports extractErrorMessage, the previously internal functionality to pull out error messages from server data.
    
    +semver:minor

--- src/check-status.js
+++ src/check-status.js
@@ -1,67 +1,36 @@
-import { get } from "lodash";
+import extractErrorMessage from "./extract-error-message";
 
-export default function checkStatus(response) {
+export default async function checkStatus(response) {
 	if (response.status >= 200 && response.status < 300) {
 		return response;
 	}
 
-	return parseJSONError(response);
+	await parseJSONError(response);
 }
 
-function parseJSONError(response) {
-	return new Promise((resolve, reject) => {
-		// try to parse the JSON before erroring
-		response
-			.json()
-			.then(data => {
-				let msg = parseErrorMessageFromData(data);
-				if (!msg) msg = response.statusText;
+async function parseJSONError(response) {
+	let data;
 
-				let error = new Error(msg);
-				error.response = {
-					status: response.status,
-					type: response.type,
-					url: response.url,
-					body: data
-				};
-
-				reject(error);
-			})
-			.catch(() => {
-				// there was an error trying to parse the JSON body (maybe it's not JSON?)
-				// just ignore it and return an error with the original response without a parsed body
-				let error = new Error(response.statusText);
-				error.response = response;
-				reject(error);
-			});
-	});
-}
-
-function parseErrorMessageFromData(data) {
-	// if it is a .net exception, try to find the inner-most exception
-	let ex = data;
-	while (ex.innerException) {
-		const origEx = ex;
-		ex = ex.innerException;
-		ex.outerException = origEx;
+	try {
+		data = await response.json();
+	} catch {
+		// there was an error trying to parse the JSON body (maybe it's not JSON?)
+		// just ignore it and return an error with the original response without a parsed body
+		let error = new Error(response.statusText);
+		error.response = response;
+		throw error;
 	}
 
-	// try to look for a message or exception message
-	let msg = getMessage(ex);
+	let msg = extractErrorMessage(data);
+	if (!msg) msg = response.statusText;
 
-	while (!msg && ex.outerException) {
-		// go back up the tree looking for more messages
-		ex = ex.outerException;
-		msg = getMessage(ex);
-	}
+	let error = new Error(msg);
+	error.response = {
+		status: response.status,
+		type: response.type,
+		url: response.url,
+		body: data
+	};
 
-	return msg;
-}
-
-function getMessage(ex) {
-	return get(
-		ex,
-		"exceptionMessage",
-		get(ex, "message", get(ex, "ExceptionMessage", get(ex, "Message")))
-	);
+	throw error;
 }
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export checkStatus from "./check-status";
 export parseJSON from "./parse-json";
 export batchFetch from "./batch-fetch";
 export fetchOnUpdate, { DisableFetchOnUpdate } from "./fetch-on-update";
+export extractErrorMessage from "./extract-error-message";
