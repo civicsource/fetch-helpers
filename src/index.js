7e41a89888b77173c3bb25732ef3bc9f8fbb7873
Chad Lee <git@chadly.net>
2019-01-15 13:44:27 -0600

    On second thought, fuck render props
    HOCs are good enough for me
    
    +semver:major

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ export checkStatus from "./check-status";
 export parseJSON from "./parse-json";
 export batchFetch from "./batch-fetch";
 export fetchOnUpdate from "./fetch-on-update";
-export Fetch from "./fetch";
 export extractErrorMessage from "./extract-error-message";
